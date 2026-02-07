3e80c09370129b5c0f1fa3c292b014d826a5164d
Christian Bates <christianbates02@gmail.com>
2026-02-07 18:25:57 +0000

    WIP: home page.

---
+++ client/src/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <div>Upload an image to generate a mosaic chart</div>
+      <input type="file" accept="image/*" />
+      <button>Generate Chart</button>
+    </>
+  )
+}
+
+export default Home
