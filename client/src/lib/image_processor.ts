612454dd5bb57be5f366dee0eba29673a56ae16f
Christian Bates <christianbates02@gmail.com>
2026-02-07 19:41:05 +0000

    refactored image processor to reduce code duplication.

--- client/src/lib/image_processor.ts
+++ client/src/lib/image_processor.ts
@@ -12,21 +12,22 @@ export class ImageProcessor {
   }
 
   getImageData(): ImageData {
+    const { ctx } = this.getCanvas();
+    return ctx.getImageData(0, 0, this.width, this.height);
+  }
+
+  toDataURL(): string {
+    const { canvas } = this.getCanvas();
+    return canvas.toDataURL();
+  }
+
+  private getCanvas() {
     const canvas = document.createElement('canvas');
     canvas.width = this.width;
     canvas.height = this.height;
     const ctx = canvas.getContext('2d');
     if (!ctx) throw new Error('Could not get 2d context');
     ctx.drawImage(this.image, 0, 0, this.width, this.height);
-    return ctx.getImageData(0, 0, this.width, this.height);
-  }
-
-  toDataURL(): string {
-    const canvas = document.createElement('canvas');
-    canvas.width = this.width;
-    canvas.height = this.height;
-    const ctx = canvas.getContext('2d');
-    ctx?.drawImage(this.image, 0, 0, this.width, this.height);
-    return canvas.toDataURL();
+    return { canvas, ctx };
   }
 }
