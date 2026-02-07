e18197457e03681e934152df514652ff6a169c55
Christian Bates <christianbates02@gmail.com>
2026-02-07 19:59:24 +0000

    implemented mosaic engine.

--- client/src/lib/mosaic_engine.ts
+++ client/src/lib/mosaic_engine.ts
@@ -1,23 +1,32 @@
 export class MosaicEngine {
     imageData: ImageData;
+    threshold: number;
 
-    constructor(imageData: ImageData) {
+    constructor(imageData: ImageData, threshold = 0.5) {
         this.imageData = imageData;
+        this.threshold = threshold;
     }
 
-    generateMosaic() {
-        const mosaic: string[] = [];
-        for (let i = 0; i < this.imageData.data.length; i += 4) {
-            const r = this.imageData.data[i];
-            const g = this.imageData.data[i + 1];
-            const b = this.imageData.data[i + 2];
-            const a = this.imageData.data[i + 3];
-            mosaic.push(this.getLuminance(r, g, b).toString());
+    /** Returns a 2D grid: mosaic[row][col] is 'B' or 'F'. */
+    generateMosaic(): string[][] {
+        const { width, height, data } = this.imageData;
+        const mosaic: string[][] = [];
+        for (let row = 0; row < height; row++) {
+            const mosaicRow: string[] = [];
+            for (let col = 0; col < width; col++) {
+                const i = (row * width + col) * 4;
+                const r = data[i];
+                const g = data[i + 1];
+                const b = data[i + 2];
+                const lum = this.getLuminance(r, g, b);
+                mosaicRow.push(lum >= this.threshold ? 'F' : 'B');
+            }
+            mosaic.push(mosaicRow);
         }
         return mosaic;
     }
 
-    private getLuminance(r: number, g: number, b: number) {
+    private getLuminance(r: number, g: number, b: number): number {
         return (0.299 * r + 0.587 * g + 0.114 * b) / 255;
     }
 }
